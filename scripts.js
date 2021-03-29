8d9a5a0e42176417957acc041720886d6ccbc6de
noutkruis <78657229+noutkruis@users.noreply.github.com>
2021-03-29 19:51:13 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const link =
-	'https://api.openweathermap.org/data/2.5/weather?q=Arnhem&units=metric&apikey=bcbe4acaf519f859739d43a16c7eae96';
+	'https://api.openweathermap.org/data/2.5/weather?q=Zevenaar&units=metric&apikey=bcbe4acaf519f859739d43a16c7eae96';
 const tempElement = document.getElementById('temp');
 const request = new XMLHttpRequest();
 request.open('GET', link, true);
@@ -8,7 +8,7 @@ request.onload = function() {
 	if (request.status >= 200 && request.status < 400) {
 		const temp = obj.main.temp;
 		console.log(temp);
-		tempElement.innerHTML = 'De temperatuur in Arnhem is ' + temp + ' &#8451';
+		tempElement.innerHTML = 'De temperatuur in Zevenaar is ' + temp + ' &#8451';
 	} else {
 		console.log('Deze plaats is onbekend');
 	}
